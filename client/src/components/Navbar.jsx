c2339700ec2316a0d7cbfed52b7bf35ad9f7365a
Ghorasaini Success <imsaphal21@gmail.com>
2023-09-21 11:05:37 +0545

    navbar is created

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,44 +1,100 @@
 import React, { Fragment, useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
-import { BsChevronDown } from "react-icons/bs";
+import { BiChevronDown } from "react-icons/bi";
 import { CgProfile } from "react-icons/cg";
 import { HiMenuAlt3 } from "react-icons/hi";
 import { AiOutlineClose, AiOutlineLogout } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { CustomButton } from ".";
+import { users } from "../utils/data";
 
+function MenuList({ user }) {
+  const handleLogout = () => {};
 
-function MenuList (){
-    const handleLogout = ()=>{
-
-    }
-
-    return (
-        <div>
-            <Menu as = "div" className = "inline-block text-left">
-                <div className="flex">
-                    <Menu.Button className="inline-flex gap-2 w-full rounded-md bg-white md:px-4 py-2 text-sm font-medium text-slate-700 hover:bg-opacity-20 ">
-
-                        <div className="leading[80px] flex flex-col items-start">
-                            <p className ="text-sm font-semibold">{user?.firstName ?? user?.name}
-                            <span className ="text-sm text-blue-600">{user?.jobTitle ?? user?.email} 
-                            
-                             </p>
-                            </span>
-
-                            </div>
-
-
-                        </MenuButton>
-                </div>
-
-                </Menu> 
-
+  return (
+    <div>
+      <Menu as="div" className="inline-block text-left">
+        <div className="flex">
+          <Menu.Button className="inline-flex gap-2 w-full rounded-md bg-white md:px-4 py-2 text-sm font-medium text-slate-700 hover:bg-opacity-20 ">
+            <div className="leading[80px] flex flex-col items-start">
+              <p className="text-sm font-semibold">
+                {user?.firstName ?? user?.name}
+              </p>
+              <span className="text-sm text-blue-600">
+                {user?.jobTitle ?? user?.email}
+              </span>
+            </div>
+            <img
+              src={user?.profileUrl}
+              alt="user profile"
+              className="w-10 h-10 rounded-full object-cover"
+            />
+            <BiChevronDown
+              className="h-8 w-8 text-slate-600"
+              aria-hidden="true"
+            />
+          </Menu.Button>
         </div>
-    )
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute z-50 right-2 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-xl focus:outline-none ">
+            <div className="p-1 ">
+              <Menu.Item>
+                {({ active }) => (
+                  <Link
+                    to={`${
+                      user?.accountType ? "user-profile" : "company-profile"
+                    }`}
+                    className={`${
+                      active ? "bg-blue-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md p-2 text-sm`}
+                    // onClick={onClick}
+                  >
+                    <CgProfile
+                      className={`${
+                        active ? "text-white" : "text-gray-600"
+                      } mr-2 h-5 w-5  `}
+                    />
+                    {user?.accountType ? "User Profile" : "Company Profile"}
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link
+                    to={`${
+                      user?.accountType ? "user-profile" : "company-profile"
+                    }`}
+                    className={`${
+                      active ? "bg-blue-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md p-2 text-sm`}
+                    onClick={handleLogout}
+                  >
+                    <AiOutlineLogout
+                      className={`${
+                        active ? "text-white" : "text-gray-600"
+                      } mr-2 h-5 w-5  `}
+                    />
+                    Log Out
+                  </Link>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
 }
 const Navbar = () => {
-  const user = {};
+  const user = users[1];
   const [isOpen, setIsOpen] = useState(false);
   const handleCloseNavbar = () => {
     setIsOpen((prev) => !prev);
@@ -68,7 +124,7 @@ const Navbar = () => {
             </li>
           </ul>
 
-          <div className="hidden lg:block">
+          <div className=" hidden lg:block">
             {!user?.token ? (
               <Link to="/user-auth">
                 <CustomButton
@@ -78,12 +134,55 @@ const Navbar = () => {
               </Link>
             ) : (
               <div>
-                <MenuList />
-                </div>
-
+                <MenuList user={user} />
+              </div>
             )}
           </div>
+
+          <button
+            className="block lg:hidden text-slate-900"
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            {isOpen ? <AiOutlineClose size={26} /> : <HiMenuAlt3 size={26} />}
+          </button>
         </nav>
+        {/* MOBILE MENU */}
+        <div
+          className={`${
+            isOpen ? " absolute flex bg-[#f7fdfd] " : "hidden"
+          } container mx-auto lg:hidden flex-col pl-8 gap-3 py-5`}
+        >
+          <Link to="/" onClick={handleCloseNavbar}>
+            Find Job
+          </Link>
+          <Link to="/companies" onClick={handleCloseNavbar}>
+            Companies
+          </Link>
+          <Link
+            to={user?.accountType == "seeker" ? "apply-gistory" : "upload-job"}
+            onClick={handleCloseNavbar}
+          >
+            {user?.accountType === "seeker" ? "Application" : "Upload Job"}
+          </Link>
+          <Link to="/about-us" onClick={handleCloseNavbar}>
+            About
+          </Link>
+
+          <div className=" w-full py-10 ">
+            {!user?.token ? (
+              <Link to="/user-auth">
+                <CustomButton
+                  title="Sign In"
+                  containerStyles="text-blue-600 py-1.5 px-5 focus:outline-none hover:bg-blue-700 hover:text-white rounded-full text-base border border-blue-600 "
+                />
+              </Link>
+            ) : (
+              <div>
+                <MenuList user={user} />
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </Fragment>
   );
